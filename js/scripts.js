554117410086ceb0aaba56cbcb7aba573c4febe9
Brian Grant and Benjamin Logue <brian@>
2016-06-14 09:24:46 -0700

    it will ad inputs for movie, showtime, and age and ticket object constructor.

--- js/scripts.js
+++ js/scripts.js
@@ -4,17 +4,35 @@ function Ticket(movie, time, age) {
     this.showtime = time;
     this.age = age;
 }
+
+Ticket.prototype.calculatePrice = function(){
+  var ticketPrice = 15;
+  if (this.showtime === "matinee") {
+    ticketPrice -= 5;
+  }
+  if (this.age === "child" || this.age === "senior") {
+    ticketPrice -= 3;
+  }
+  return ticketPrice;
+}
 // Calls input from form-input.html
 
 $(document).ready(function(){
 
-  $("#input").submit(function(event){
+  $("#order-ticket").submit(function(event){
     event.preventDefault();
-    var input = ($("#blank").val());
+    var selectedMovie = $("#movies option:selected" ).val();
+    var selectedTime = $('input[name=show-time]:checked').val()
+    var selectedAge = $("#age option:selected").val();
 
-    var output = "";
+    var ticketPurchase = new Ticket(selectedMovie, selectedTime, selectedAge);
 
-    $('#output').text(output);
+    ticketPurchase.calculatePrice();
+
+    $("#output").append("<li>")
+
+
+
 
   });
 });
